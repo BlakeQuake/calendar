4d9d20f0bc45268c85f98acdd523be0725bd65ea
Blake Lampkin <blakequake@BlakeQuakes-UFO.lan>
2020-08-05 15:39:33 -0700

    got my live calendar working

--- Develop/script.js
+++ Develop/script.js
@@ -5,18 +5,50 @@
 // else if .hour > currentime, then future (green).
 // else if .hour < currentime time, then past (gray)
 
+// // Javascript vanilla way
+// var currentDay = document.querySelector("#currentDay");
 
+/////////// MOMENT TIMER ///////////////////////
 
-
-
-
-
-// Javascript vanilla way
-var currentDay = document.querySelector("#currentDay");
-
-// jQuery way
-var momentTimer = moment().format('MMM do YYYY, h:mm:ss a');
+var momentTimer = moment().format('MMM do YYYY, h:mm A');
 var currentDay = $("#currentDay")
 $("#currentDay").append(momentTimer)
 console.log(momentTimer)
 
+///////////// LIVE CLOCK ///////////////////////
+
+var currentHour = moment().hour()
+console.log(moment().hour())
+
+for (var i = 0; i < 18; i++) {
+    if (i < currentHour )
+    {
+        $("#" + i).addClass("past")
+    }
+    else if (i === currentHour){
+        $("#" + i).addClass("present")
+    }
+    else {
+        $("#" + i).addClass("future")
+    }
+}
+
+
+var nine 
+var ten
+var eleven
+var twelve = ".twelve"
+var one
+var two
+var three
+var four
+var five 
+
+///////////// LOCAL STORAGE ///////////////////
+
+localStorage.setItem(".twelve", twelve)
+console.log(twelve)
+
+$(".saveBtn").on("click", function() {
+    
+})
