40400c42f93e0fb08a995fd5f3980cff2dc3cb3f
Blake Lampkin <blakequake@BlakeQuakes-UFO.lan>
2020-08-04 20:06:41 -0700

    got text inside of boxes

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,19 @@
-var d = new Date();
-document.getElementById("currentDay").innerHTML = d;
+
+// counter with current date and timer (moment.js)
+// place cunter inside the jumbotron
+// if statement - if .hour = currenttime, then present(red).
+// else if .hour > currentime, then future (green).
+// else if .hour < currentime time, then past (gray)
+
+
+
+
+
+
+
+var momentTimer = moment().format('MMM do YYYY, h:mm:ss a');
+var currentDay = document.querySelector("#currentDay");
+
+
+function$(currentDay).append(momentTimer)
+console.log(currentDay)
